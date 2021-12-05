17ba4bb868d35c26c20eac5eba0efd8d9593feb3
jakobskr <jakob2407@gmail.com>
2021-12-05 16:11:44 +0100

    finished polishing

--- day5.py
+++ day5.py
@@ -7,35 +7,23 @@ def newRange(x,y):
         return tmp
 
 cordinates = {}
-
 for line in open("day5_input"):
     cords = line.strip().split(" -> ")
     x1,y1 = [int(y) for y in cords[0].split(",")]
     x2, y2 = [int(y) for y in cords[1].split(",")]
-
-    if x1 == x2:
+    if x1 == x2 or y1 == y2:
         for i in newRange(y1,y2 + 1):
-            if "({},{})".format(i, x1) in cordinates:
-                cordinates["({},{})".format(i,x1)] += 1
-            else:
-                cordinates["({},{})".format(i,x1)] = 1
-    elif y1 == y2:
-        for i in newRange(x1,x2 + 1):
-            if "({},{})".format(y1,i) in cordinates:
-                cordinates["({},{})".format(y1,i)] += 1
-            else:
-                cordinates["({},{})".format(y1,i)] = 1
+            for j in newRange(x1,x2 + 1):
+                if "({},{})".format(i, j) in cordinates:
+                    cordinates["({},{})".format(i,j)] += 1
+                else:
+                    cordinates["({},{})".format(i,j)] = 1
     else:
-        print(x1, ":", y1 , "->" , x2, ":", y2)
-        cords = zip(newRange(x1,x2 + 1), newRange(y1, y2 + 1))
-        print(list(newRange(x1,x2 + 1)), list(newRange(y1, y2 + 1)))
-        for cord in cords:
-            #print(cord)
+        for cord in zip(newRange(x1,x2 + 1), newRange(y1, y2 + 1)):
             if "({},{})".format(cord[1], cord[0]) in cordinates:
                 cordinates["({},{})".format(cord[1], cord[0])] += 1
             else:
                 cordinates["({},{})".format(cord[1], cord[0])] = 1
 
 num = len([x for x in cordinates.values() if x > 1])
-
 print("amount of intersections: ", num)
