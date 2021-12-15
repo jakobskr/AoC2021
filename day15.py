7d7a7a5ca9de98b592108b9fce139872ef1af62b
jakobskr <jakob2407@gmail.com>
2021-12-15 19:29:25 +0100

    im to dumb for this shit apperantly ...

--- day15.py
+++ day15.py
@@ -1,6 +1,8 @@
 
 from typing import Iterable
+from collections import defaultdict
 import numpy as np
+import heapq as hq
 
 def printArray(array):
     for row in array:
@@ -8,65 +10,68 @@ def printArray(array):
         for x in row:
             rep += str(x) + " "
         print(rep)
-    print(rep)
+    print()
 
-def shortDist(graph, distance):
-    cord = graph[0]
-    low = distance[cord[1]][cord[0]]
-
-    for y,row in enumerate(distance):
-        for x, val in enumerate(row):
-            if (x,y) in graph:
-                if val < low:
-                    val = low
-                    cord = (x,y)
-    return cord
+def solve(tiles):
+    largeCave = []
+    paths = []
+    cave = []
+    height, width = 0,0
     
+    for line in open("day15_input","r"):
+        row = []
+        for l in line.strip():
+            row.append(int(l))
+        cave.append(row)
 
-paths = []
-cave = []
-height, width = 0,0
-validNeighbours = lambda x, y : {(max(x - 1, 0),y) , (min(x + 1, width), y), (x, max(y - 1, 0)), (x, min(y + 1, height))}
+    height = len(cave)
+    width = len(cave[0])
 
+    for y in range(height * tiles):
+        row = []
+        for x in range(width * tiles):
+            val = cave[y % height][x % width] + x // width + y // height
+            row.append((val - 1) % (9) + 1)
+        largeCave.append(row)
 
-for line in open("day15_input","r"):
-    row = []
-    for l in line.strip():
-        row.append(int(l))
-    cave.append(row)
-print (cave)
+    width *= tiles
+    height *= tiles
 
-height = len(cave)
-width = len(cave)
-
-print(validNeighbours(3,3))
-print("w:",width, "h",height)
-# begin djikstra
-distance = [[9999999 for x in range(width)] for y in range(height)]
-graph = [(x,y) for x in range(width) for y in range(height)]
-prev = [[0 for x in range(width)] for y in range(height)]
-end = (width - 1, height - 1)
-distance[0][0] = 0
-print(graph)
-print(shortDist(graph, distance))
-pop = lambda : graph[distance.index(min(distance))]
-print(pop())
-
-while graph:
-    u = shortDist(graph,distance)
-
-    graph.remove(u)
-
-    print(u)
-    if u == end:
-        print("found path")
-        break
+    validNeighbours = lambda x, y : {(max(x - 1, 0),y) , (min(x + 1, width - 1), y), (x, max(y - 1, 0)), (x, min(y + 1, height - 1))}
+    cave = largeCave
+    # begin djikstra
     
-    for x,y in validNeighbours(*u,):
-        if (x,y) in graph:
-            alt = distance[u[1]][u[0]] + cave[y][x]
-            if alt < distance[y][x]:
-                distance[y][x] = alt
-                prev[y][x] = u
-printArray(distance)
-printArray(prev)
+    distance = [[9999999 for x in range(width)] for y in range(height)]
+    graph = [(x,y) for x in range(width) for y in range(height)]
+    end = (width - 1, height - 1)
+    distance[0][0] = 0
+
+    queue = []
+    hq.heappush(queue,(0,(0,0)))
+    visited = set()
+
+    while graph:
+        #print(queue)
+        dist, node = hq.heappop(queue)
+
+        if node == end:
+            break
+
+        if node in visited:
+            continue
+
+        visited.add(node)
+        nx,ny = node
+
+        for x,y in validNeighbours(*node):
+            if (x,y) not in visited:
+                alt = dist + cave[y][x]
+                if alt < distance[y][x]:
+                    distance[y][x] = alt
+                    hq.heappush(queue, (alt, (x,y)))
+
+    print(distance[-1][-1])
+
+
+solve(1)
+solve(5)
